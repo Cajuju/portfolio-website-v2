a11e243b904c9c2cbbb70098cfe08e3a9b8c1258
Brian Cajulis <bcajulis12@apu.edu>
2018-12-13 12:49:59 -0800

    Text Changes & Style Adjustments

--- src/containers/about/About.js
+++ src/containers/about/About.js
@@ -6,7 +6,7 @@ class About extends Component {
         return (
             <div className='about'>
                 <p>
-                Brian Robert Cajulis is a web/mobile developer who specializes in front end development and is based in Los Angeles, CA.
+                Brian Robert Cajulis is a web/mobile developer who specializes in front end development and is based in Reseda, CA.
                 Brian focuses on creating applications that function well according to their specifications and have a fully responsive interface that looks consistent on a wide range of devices.
                 He began pursuing a career in software development while attending <label onClick={() => window.open("https://www.apu.edu/", '_blank')} style={{textDecoration: 'underline'}}>Azusa Pacific University</label>, and he graduated with BS in Computer Science on May 2018. During his college career, he was part of the first computer science action team from his university which traveled to Namibia, South Africa.
                 Whilst there, he assisted students and professors from <label onClick={() => window.open('http://www.nust.na/', '_blank')} style={{textDecoration: 'underline'}}>Namibia University of Science and Technology</label> to create a health informatics system for public and private hospitals.
